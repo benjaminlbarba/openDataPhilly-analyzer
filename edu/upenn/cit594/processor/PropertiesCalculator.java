518c0a166924dff588ea7164fd5803898b30720a
Benjamin Barba <benbarba@seas.upenn.edu>
2020-04-19 21:54:00 -0700

    Adding PropertiesCalculator class - empty rn just stubbing in

---
+++ edu/upenn/cit594/processor/PropertiesCalculator.java
@@ -0,0 +1,18 @@
+package edu.upenn.cit594.processor;
+
+public class PropertiesCalculator {
+	/*
+	
+	The average residential market value that your program displays must be truncated to
+	an integer (not rounded!), and your program should display 0 if the total residential
+	market value for the ZIP Code is 0, or if the user enters an input that is not a valid ZIP
+	Code or is not a ZIP Code that is listed in the input files.
+	Your program must not write any other information to the console. It must only
+	display the average residential market value for the specified ZIP Code and then the
+	program should prompt for another input in Step #0.*/
+	
+	//option 3 - prompt the user to enter a zip code
+	
+	//print out the average residential market value for that ZIP Code, 
+	//i.e. the total market value for all residences in the ZIP Code divided by the number of residences.
+}
