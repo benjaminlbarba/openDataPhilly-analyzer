dfef9e90bbfddeea6a87a04c5bac087512f09c61
Benjamin Barba <benbarba@seas.upenn.edu>
2020-04-19 21:01:30 -0700

    Adds UserInput class and further work on Main

--- edu/upenn/cit594/Main.java
+++ edu/upenn/cit594/Main.java
@@ -1,5 +1,6 @@
 package edu.upenn.cit594;
 
+import src.edu.upenn.cit594.ui.UserInput;
 
 /**
  * 
@@ -10,6 +11,7 @@ package edu.upenn.cit594;
 
 public class Main {
 	static final int NUM_ARGS = 5;
+	
 	/**
 	 * reads runtime arguments and performs operations using each argument
 	 * @param args - runtime arguments.  Expects five: 
@@ -19,8 +21,33 @@ public class Main {
 	 *  3 The name of the population input file
 	 *  4 The name of the log file (described below)
 	 */
+	public static void inputHandler(int input) {
+		switch(input) {
+		  case 0:
+			//TODO: exit program
+		    break;
+		  case 1:
+		    // TODO: show the total population for all ZIP Codes, as described in Step #1 below.
+		    break;
+		  case 2:
+			// TODO:show the total parking fines per capita for each ZIP Code, as described in Step #2 below.
+			break;
+		  case 3:
+			// TODO:show the average market value for residences in a specified ZIP Code, as described in Step #3 below.
+			break;
+		  case 4:
+			// TODO:show the average total livable area for residences in a specified ZIP Code, as described in Step #4
+			break;
+		  case 5:
+			// TODO: show the total residential market value per capita for a specified ZIP Code, as described in Step #5 below.
+		  case 6:
+			// TODO:  show the results of your custom feature, as described in Step #6 below 
+		  default:
+		    // code block
+		}
+	}
 	
-	private static void argumentHandler(String[] args) {
+	private static int argumentHandler(String[] args) {
 		for (int i = 0; i < args.length; i++) {
 			System.out.println("args[" + i + "] = " + args[i]);
 		}
@@ -32,14 +59,25 @@ public class Main {
 			String fileNamePopulation = args[3];
 			String fileNameLog = args[4];
 			
+			if (!fileType.equalsIgnoreCase("CSV") && !fileType.equalsIgnoreCase("JSON")){
+				return -1;
+			}
+			
+			//Read user input
+			int input = UserInput.readingUserInput();
+			inputHandler(input);
+			
 			//TODO: extract and process data
-	
+			
+			
 			//TODO: display and log
 			
 		}
 		else {
 			System.out.println("Should have four arguments.  Found " + args.length + " out of " + NUM_ARGS);
 		}
+		
+		return 0;
 	}
 	
 	
