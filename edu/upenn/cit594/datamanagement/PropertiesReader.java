be59eed43ef90d04d7d5208b705ccb9ad568b9c3
Benjamin Barba <bbarba@imbellus.com>
2020-04-25 20:32:53 -0700

    Properties csv reader class works - and removed main and printlns from that file

--- edu/upenn/cit594/datamanagement/PropertiesReader.java
+++ edu/upenn/cit594/datamanagement/PropertiesReader.java
@@ -26,15 +26,17 @@ public class PropertiesReader {
 			int indexGarageSpaces = -1;
 			int indexZipcode = -1;
 			
+			int lnCounter = 0;
 			while (in.hasNext()) {
 				//[41.38, -81.49] 6 2019-01-28 19:02:28 Yay, homework!
 				String line = in.nextLine();
-				String[] entries = line.split(",");
+				String[] entriesOriginal = line.split(",");
 				
+				//do first time for first line
 				if (bLabelsLine) {
-					System.out.println(line);
-					for (int i = 0; i < entries.length; i++) {
-						String entry = entries[i];
+					//System.out.println(line);
+					for (int i = 0; i < entriesOriginal.length; i++) {
+						String entry = entriesOriginal[i];
 						if (entry.equalsIgnoreCase("market_value")) {
 							indexMarketValue = i;
 						}
@@ -50,47 +52,96 @@ public class PropertiesReader {
 					}
 					
 					bLabelsLine = false;
+					continue;
 				}
-				else {
-					String marketValueString = entries[indexMarketValue];
-					Double marketValue = -1.0;
-					if (!marketValueString.isEmpty()) {
-						try{
-							marketValue = Double.parseDouble(marketValueString);
-						}
-						catch(NumberFormatException e){
-							continue;
-						}
-					}
-					String totalLivableAreaString = entries[indexTotalLivableArea];
-					Double totalLivableArea = -1.0;
-					if (!totalLivableAreaString.isEmpty()) {
-						try {
-							totalLivableArea = Double.parseDouble(totalLivableAreaString);
-						}
-						catch(NumberFormatException e){
-							continue;
-						}
-					}
-					String garageSpacesString = entries[indexGarageSpaces];
-					int garageSpaces = -1;
-					if (!garageSpacesString.isEmpty()) {
-						try {
-							garageSpaces = Integer.parseInt(garageSpacesString);
-						}
-						catch(NumberFormatException e){
-							continue;
-						}
-					}
-					String zipcode = entries[indexZipcode];
-					if (Property.isValidZipCode(zipcode)){
-						zipcode = zipcode.substring(0, 5);
+				
+				String[] entries = new String[entriesOriginal.length];
+				
+				String concatenatedEntry = "";
+				boolean bConcat = false;
+				int indexEntriesFinal = 0;
+				for (int i = 0; i < entriesOriginal.length; i++) {
+					String entry = entriesOriginal[i];
+					
+					for (String entryOriginal : entriesOriginal) {
+						//System.out.println("entryOriginal = " + entryOriginal);
 					}
 					
-					Property newProperty = new Property(marketValue, totalLivableArea, garageSpaces, zipcode);
+					//if (!entry.isEmpty()) {
+					if (bConcat) {
+						
+						concatenatedEntry = concatenatedEntry.concat(entry);
+						System.out.println("... " + concatenatedEntry);
+						//ending quotation mark found
+						//stop concatenating and reset
+						if (entry.matches("$\"")&& bConcat) {
+							bConcat = false;
+							entries[indexEntriesFinal] = concatenatedEntry;
+							indexEntriesFinal += 1;
+							concatenatedEntry = "";
+						}
+					}
+					//start concatenating
+					else if (entry.matches("^\"") && (!entry.matches("^\"$"))) {
+						System.out.println("concat start [" + lnCounter+ "]= " + entry);
+						bConcat = true;
+						concatenatedEntry = concatenatedEntry.concat(entry);
+						continue;
+					}
+					//keep adding normally
+					else {
+						//System.out.println("adding entry = " + entry);
+						entries[indexEntriesFinal] = entry;
+						indexEntriesFinal += 1;
+						
+					}
 					
-					properties.add(newProperty);
 				}
+				lnCounter++;
+				
+
+//				for (String entry : entries) {
+					//System.out.println("entry = " + entry);
+//				}
+				
+				String marketValueString = entries[indexMarketValue];
+				Double marketValue = -1.0;
+				if (!marketValueString.isEmpty()) {
+					try{
+						marketValue = Double.parseDouble(marketValueString);
+					}
+					catch(NumberFormatException e){
+						continue;
+					}
+				}
+				String totalLivableAreaString = entries[indexTotalLivableArea];
+				Double totalLivableArea = -1.0;
+				if (!totalLivableAreaString.isEmpty()) {
+					try {
+						totalLivableArea = Double.parseDouble(totalLivableAreaString);
+					}
+					catch(NumberFormatException e){
+						continue;
+					}
+				}
+				String garageSpacesString = entries[indexGarageSpaces];
+				int garageSpaces = -1;
+				if (!garageSpacesString.isEmpty()) {
+					try {
+						garageSpaces = Integer.parseInt(garageSpacesString);
+					}
+					catch(NumberFormatException e){
+						continue;
+					}
+				}
+				String zipcode = entries[indexZipcode];
+				if (Property.isValidZipCode(zipcode)){
+					zipcode = zipcode.substring(0, 5);
+				}
+				
+				Property newProperty = new Property(marketValue, totalLivableArea, garageSpaces, zipcode);
+				
+				properties.add(newProperty);
 			}
 			
 			in.close();
@@ -103,13 +154,6 @@ public class PropertiesReader {
 //			Property property = properties.get(i);
 //			System.out.println("Property["  + i + "]: " + "zipcode = " + property.getZipcode() + ", mv = " + property.getMarketValue() + ", gs = " + property.getGarageSpaces() );
 //		}
-		
-		
 		return properties;
 	}
-	
-	public static void main(String[] args) {
-		PropertiesReader.read("properties.csv");
-	}
-
 }
