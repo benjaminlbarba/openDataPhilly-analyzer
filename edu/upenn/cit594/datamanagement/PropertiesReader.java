82f8c0dab1e0f17fc3c90a3f062746d82e9d21b4
Benjamin Barba <bbarba@imbellus.com>
2020-04-25 18:07:24 -0700

    removing uneeded imports from propertyreader class

--- edu/upenn/cit594/datamanagement/PropertiesReader.java
+++ edu/upenn/cit594/datamanagement/PropertiesReader.java
@@ -3,16 +3,10 @@ package edu.upenn.cit594.datamanagement;
 import java.io.File;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Scanner;
 
 import edu.upenn.cit594.data.Property;
-import edu.upenn.cit594.logging.UserInfoLogger;
 
 public class PropertiesReader {
 	
